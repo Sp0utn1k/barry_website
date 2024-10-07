cd40dd2fba63e92360d394ede88f17551cab9867
Sp0utn1k <maximilienremacle@gmail.com>
2024-10-07 18:44:35 +0200

    website enhancements

---
+++ menu.js
@@ -0,0 +1,17 @@
+function toggleMobileMenu() {
+    var menu = document.getElementById('mobileMenu');
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+}
+
+function toggleSubMenu() {
+    var submenu = document.getElementById('mobileSubMenu');
+    if (submenu.style.display === 'block') {
+        submenu.style.display = 'none';
+    } else {
+        submenu.style.display = 'block';
+    }
+}
---
+++ scripts.js
@@ -0,0 +1,28 @@
+function updateSliderBackground(slider, color) {
+    var value = slider.value;
+    var min = slider.min;
+    var max = slider.max;
+    var percentage = (value - min) / (max - min) * 100;
+
+    /* For WebKit browsers */
+    var bg = 'linear-gradient(to right, ' + color + ' 0%, ' + color + ' ' + percentage + '%, #cccccc ' + percentage + '%, #cccccc 100%)';
+    slider.style.background = bg;
+
+    /* For Firefox */
+    slider.style.setProperty('--range-progress', percentage + '%');
+}
+
+var sliders = [
+    {id: 'slider-red', color: '#a50f01'},
+    {id: 'slider-green', color: '#299e37'},
+    {id: 'slider-blue', color: '#4d8dd6'},
+    {id: 'slider-warm-white', color: '#ffeac1'}
+];
+
+sliders.forEach(function(s) {
+    var slider = document.getElementById(s.id);
+    updateSliderBackground(slider, s.color);
+    slider.addEventListener('input', function() {
+        updateSliderBackground(slider, s.color);
+    });
+});
